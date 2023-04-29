d0114b44f0f36e843c31b780f12cedc8355dbf7a
Eduardo Ismael García Pérez <eduardo78d@gmail.com>
2023-04-28 18:47:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,87 +1,122 @@
 import csv
 import requests
-from bs4 import BeautifulSoup 
-from collections import namedtuple
+from bs4 import BeautifulSoup
 
+URL = 'https://www.imdb.com/calendar/?region=MX'
 
-URL = 'https://www.imdb.com/calendar/?region=MX&type=MOVIE'
-IMDB_FILE = 'imdb.txt'
+"""
+1.- Obtener el maqueto HTML
+    - Si el archivo HTML no existe de forma local, crearlo.
+    - Si el archivo HTML existe de forma local, obtener su contenido.
+2.- Obtener la información
+    - Nombre
+    - Categorias
+    - Reparto
+3.- Generar un archivo CSV
+"""
 
-def generate_request() -> str:
+def get_imdb_content():
+    """Get the content of the IMDB calendar page
+
+    Returns:
+        string -- The content of the IMDB calendar page
+        None -- If the request was not successful
+    """
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0'
     }
 
-    response = requests.get(URL, headers=headers)
+    response = requests.get(URL, headers=headers) # 20x - 30x - 40x - 50x
     if response.status_code == 200:
         return response.text
-
-    return None
-
-
-def read_imdb_file() -> str:
-    try:
-        with open(IMDB_FILE) as file:
-            return file.read()
     
-    except Exception as err:
-        return None
+    return None
+    
 
-
-def imdb_content() -> str:
-    content = read_imdb_file()
-    if not content:
-        content = generate_request()
-
-        with open(IMDB_FILE, 'w') as file:
+def create_imdb_file_local(content):
+    """Crear un archivo local a partir del maquetado de un sitio web."""
+    try:
+        with open('imdb.html', 'w') as file:
             file.write(content)
+    except:
+        pass
+
+
+def get_imdb_file_local():
+    """Lee el contenitdo de un archivo local"""
+    content = None
+    
+    try:
+        with open('imdb.html', 'r') as file:
+            content = file.read()
+    except:
+        pass
     
     return content
 
 
-def generate_string_points(tag_list):
-    return ','.join([ li.span.text for li in tag_list.find_all('li') ])
-
-
-def generate_movie(li_tag) -> None:
-    div_tag = li_tag.find('div', 
-        {'class': 'ipc-metadata-list-summary-item__c'}
-    )
-
-    name = div_tag.div.a.text
-    details = div_tag.find_all('ul')
-
-    categories = generate_string_points(details[0])
-    actors = generate_string_points(details[1]) if len(details) == 2 else 'None'
+def get_local_imdb_content():
+    """Obtiene el contenido del maquetado de Imdb. Ya sea de forma local o del servidor"""
+    content = get_imdb_file_local()
     
-    movie = namedtuple('Movie', ['name', 'categories', 'actors'])
-    return movie(name, categories, actors)
-   
+    if content:
+        return content
+
+    content = get_imdb_content()
+    create_imdb_file_local(content)
+
+    return content
+    
+
+
+def create_movie(tag):
+    main_div = tag.find('div', {'class': 'ipc-metadata-list-summary-item__c' })
+        
+    name =  main_div.div.a.text
+    ul_categories = main_div.find('ul', {
+        'class': 'ipc-inline-list ipc-inline-list--show-dividers ipc-inline-list--no-wrap ipc-inline-list--inline ipc-metadata-list-summary-item__tl base'
+    })
+
+    ul_cast = main_div.find('ul', {
+        'class': 'ipc-inline-list ipc-inline-list--show-dividers ipc-inline-list--no-wrap ipc-inline-list--inline ipc-metadata-list-summary-item__stl base'
+    }) # None
+
+    cast = None
+    categories = [ category.span.text for category in  ul_categories.find_all('li') ]
+    
+    cast = [ cast.span.text for cast in ul_cast.find_all('li') ] if ul_cast else []
+    
+    return (name, categories, cast) # Retornamos un tupla
+
+
 
 def main():
-    content = imdb_content()
-
-    if not content:
-        return False
-
+    content = get_local_imdb_content()
+    
+    # HTML
     soup = BeautifulSoup(content, 'html.parser')
-    li_tags = soup.findAll('li', {
+    
+    li_tags = soup.find_all('li', {
         'data-testid': 'coming-soon-entry',
         'class': 'ipc-metadata-list-summary-item ipc-metadata-list-summary-item--click sc-8c2b7f1f-0 bpqYIE'
     })
 
-    movies = [ generate_movie(movie) for movie in li_tags]
-    generate_csv(movies)
-
-
-def generate_csv(movies):
+    movies = []
+    for tag in li_tags:
+        movie = create_movie(tag)
+        movies.append(movie)
 
     with open('movies.csv', 'w') as file:
-        writer = csv.writer(file)
-        writer.writerow( ['name', 'categories', 'actors']) 
+        writer = csv.writer(file, delimiter="-")
+        writer.writerow(['name', 'categories', 'cast'])
 
         for movie in movies:
-            writer.writerow(movie)
+            writer.writerow([
+                movie[0], # name
+                ",".join(movie[1]), # categories
+                ",".join(movie[2]), # cast
+            ])
+
 
 
 if __name__ == '__main__':
